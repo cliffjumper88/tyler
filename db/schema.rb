a223b2757e5b9a2c2b43f87bf964f46b69c6b072
Tyler <lschaeferaz@hotmail.com>
2015-07-09 19:15:47 -0400

    Perhaps deleting sql from gemfile.lock will help heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706185559) do
+ActiveRecord::Schema.define(version: 20150709121424) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture"
+    t.string   "url"
   end
 
 end
